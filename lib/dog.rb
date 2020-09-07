edb827ae67f71a974261fb70b905f77f1765dd07
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-09-07 21:08:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -96,5 +96,9 @@ class Dog
     if !dog.empty?
       dog_data = dog[0]
       dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+    else
+      dog = self.create(name: name, breed: breed)
+    end
+    dog
   end
 end 
