863957363534652f702c64454e4fb155a765b5a0
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-09-07 20:13:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,4 +11,29 @@ class Dog
     @breed = breed
   end
   
+  def self.create_table
+    sql = <<-SQL
+    "CREATE TABLE IF NOT EXIST dogs(
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT"
+    SQL
+    DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
+  end
+  
+  def self.new_from_db
+  end
+  
+  def self.find_by_name
+  end
+  
+  def self.update
+  end
+  
+  def self.save
+  end
+  
 end 
