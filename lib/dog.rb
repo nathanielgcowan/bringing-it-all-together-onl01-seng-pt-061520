d0cd5f0b5e84aa2be9481e8171685f9ece474bd6
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-09-07 20:58:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -53,6 +53,17 @@ class Dog
   end
   
   def save
+      if self.id
+        self.update
+      else
+        sql = <<-SQL
+          INSERT INTO dogs (name, breed)
+          VALUES (?, ?)
+        SQL
+        DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")
+      end
+      self
+    end
   end
   
 end 
