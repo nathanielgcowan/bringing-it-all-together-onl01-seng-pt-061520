9f925257745583564f71ec1efd63ef3ff0266a2d
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-09-07 20:19:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -27,7 +27,11 @@ class Dog
     DB[:conn].execute(sql)
   end
   
-  def self.new_from_db
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(id: id, name: name, breed: breed)
   end
   
   def self.find_by_name
