cc48f1e3562b189492828cb6a288b42d22bd002a
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-09-07 20:09:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,4 +3,12 @@ require_relative "../config/environment.rb"
 
 class Dog 
 
+  attr_accessor :id, :name, :breed
+  
+  def initialize(id: nil, name:, breed)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+  
 end 
