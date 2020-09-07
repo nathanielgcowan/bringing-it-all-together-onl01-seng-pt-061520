fee20ee84c7def64e51f95658ccc2fcb42adacca
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-09-07 20:24:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -41,7 +41,8 @@ class Dog
       WHERE name = ?
       LIMIT 1
     SQL
-    DB[:conn].execute (sql, name).map do |row|
+    
+    DB[:conn].execute (sql,name).map do |row|
       self.new_from_db(row)
     end.first
   end
